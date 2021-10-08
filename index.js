1d793060aa049fbbbd4c5c456dd08455f61f0277
rsuriano <ramirosuriano@gmail.com>
2021-10-08 17:57:43 -0300

    3.15: Phonebook database, step3

--- index.js
+++ index.js
@@ -14,67 +14,42 @@ const getBody = (request, response) => {
 morgan.token('body', getBody)
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-let entries = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+let entries = []
 
-// Backend landing page
-app.get('/', (request, response) => {
-    response.send("<h1>phonebook api</h1>")
-})
-
-// Backend info
-app.get('/info', (request, response) => {
-    const resHTML = `<div> Phonebook has info for ${entries.length} people. <br /> ${Date()} </div>`
-    response.send(resHTML)
-})
+// // Backend info
+// app.get('/info', (request, response) => {
+//     const resHTML = `<div> Phonebook has info for ${entries.length} people. <br /> ${Date()} </div>`
+//     response.send(resHTML)
+// })
 
 // Get all entries
 app.get('/api/persons', (request, response) => {
     Entry.find({}).then(entries => {
         response.json(entries)
-        mongoose.connection.close()
     })
 })
 
 // Get entry by id
 app.get('/api/persons/:id', (request, response) => {
-    Entry.findById(request.params.id).then(note => {
-        response.json(note)
-        mongoose.connection.close()
-    })
+    Entry.findById(request.params.id)
+        .then(entry => {
+            if (entry) {
+                response.json(entry)
+            }   else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
 // Delete entry by id
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    entries = entries.filter(entry => entry.id !== id)
-
-    response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+    Entry.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
-
-const generateRandomId = () => {
-    return Math.floor(Math.random() * 99999);
-}  
 
 // Add entry to the phonebook
 app.post('/api/persons', (request, response) => {
@@ -89,16 +64,30 @@ app.post('/api/persons', (request, response) => {
     // }
 
     const newEntry = new Entry({
-        id: generateRandomId(),
         name: data.name,
         number: data.number
     })
     newEntry.save().then( result => {
         response.json(result)
-        mongoose.connection.close()
     })
 })
 
+// Error handler middlewares
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name == 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
